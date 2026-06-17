18d0ee1e5353bbef5af995f1cd12d0644f898527
fennerii <jonathanfennerii@gmail.com>
2026-06-17 15:36:31 -0400

    imports for disposal

--- server/disposal.py
+++ server/disposal.py
@@ -11,3 +11,12 @@ from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, BS
 #Docs: https://python.langchain.com/docs/integrations/document_loaders/
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+# Splits largs docs into smaller chunks to be processed by the LLM
+# Docs: https://python.langchain.com/docs/how_to/recursive_text_splitter/
+
+from langchain_chroma import Chroma
+# Vector Sotre - stores documents and chunks as vectors
+# Docs: https://python.langchain.com/docs/integrations/vectorstores/chroma/
+
+from langchain_ollama import OllamaEmbeddings, OllamaLLM
+# OllamaEmbeddings -> Convers chunks into vectors locally using llama3.2
