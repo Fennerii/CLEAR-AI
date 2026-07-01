dd66d9107dbc262d4ab7767614a655a2fa0a7c8e
fennerii <jonathanfennerii@gmail.com>
2026-07-01 14:41:44 -0400

    train and models

---
+++ datasets/README.dataset.txt
@@ -0,0 +1,6 @@
+# Clear Beta v.1 > 2026-07-01 2:28pm
+https://universe.roboflow.com/jonathanfennerii-gmail-com/clear-beta-v-1
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+model = YOLO("yolo26n.pt")
+
+results = model.train(
+    data="datasets/combined/data.yaml",
+    epochs=100,
+    imgsz=640,
+    batch=32,
+    exist_ok=True,
+    name="train_combined"
+)
+
+model.val()
